875833756cde69dba9ceb780316810b2ec38bf61
andrewray <evilkidder@gmail.com>
2023-04-18 19:46:55 +0100

    _

--- jpeg/hardcaml/src/bitstream_reader.ml
+++ jpeg/hardcaml/src/bitstream_reader.ml
@@ -43,65 +43,100 @@ end
 
 (* Another way of thinking about this. 
    
-  We have a 40 bit shift register formed of 5 bytes 
+  We have a 40 bit shift register.  It shifts up in increments of 8 or 16 bits.
 
-  Data enters at bytes [3][4].  No shifting required, but we will need to route 
-  bytes to the correct place.  If we're removing stuffing bytes, we might need 
-  to only write to one or the other location.
+  We use the top 24 (actually 23) bits to hold the (shifted) 16 bit output.  So we need
+  to shift the output by 0..7 bits.
 
-  We have an 8 bit shift window out from bytes [0][1][2].  As we read we shift down by 1 or 2
-  places and update the shift offset.
-
-  [0][1][2][3][4]
-
-  Although no shifting on the output would be nice, 8 places (hopefully we only need 0..7) should
-  be relatively light - perhaps just 1 or 2 luts, which is much better than the current design.
+  16 bit input data enters at the bottom 24 bits (though at a byte boundary) depending 
+  the current shift offset.
 *)
 
 module Var = Always.Variable
 
+module State = struct
+  type t =
+    | Pass
+    | Load
+  [@@deriving sexp_of, compare, enumerate]
+end
+
+type reg_with_next =
+  { reg : Var.t
+  ; next : Var.t
+  }
+
+let reg_with_next clocking ~name ~width =
+  let reg = Clocking.Var.reg clocking ~width in
+  let next = Var.wire ~default:reg.value in
+  ignore (reg.value -- name : Signal.t);
+  ignore (next.value -- (name ^ "_next") : Signal.t);
+  { reg; next }
+;;
+
 let create scope (i : _ I.t) =
   let ( -- ) = Scope.naming scope in
   let vname (v : Var.t) name = ignore (v.value -- name : Signal.t) in
-  let buffer = Clocking.Var.reg i.clocking ~width:40 in
-  vname buffer "buffer";
+  let sm = Always.State_machine.create (module State) (Clocking.to_spec i.clocking) in
+  ignore (sm.current -- "STATE" : Signal.t);
+  let buffer = reg_with_next i.clocking ~name:"buffer" ~width:40 in
   let shift_offset = Clocking.Var.reg i.clocking ~width:3 in
   vname shift_offset "shift_offset";
-  let shift_offset_next =
-    uresize shift_offset.value 5 +: i.read_entropy_bits -- "shift_offset_next"
-  in
-  let bits = log_shift sll (sel_top buffer.value 24) shift_offset.value in
-  let one_byte_shifted = Clocking.Var.reg i.clocking ~width:1 in
-  vname one_byte_shifted "one_byte_shifted";
+  let shift_offset_next = reg_with_next i.clocking ~name:"shift_offset_next" ~width:5 in
+  let bits = log_shift sll (sel_top buffer.reg.value 24) shift_offset.value in
+  let one_byte_shifted = reg_with_next i.clocking ~name:"one_byte_shifted" ~width:1 in
+  let read = Var.wire ~default:gnd in
   let jpeg_ready = Var.wire ~default:gnd in
-  let _bits_valid = Var.wire ~default:gnd in
+  let bits_valid = Var.wire ~default:gnd in
+  (* combinational shift register logic *)
   Always.(
     compile
-      [ shift_offset <-- shift_offset_next.:[2, 0]
+      [ shift_offset_next.next <-- uresize shift_offset.value 5 +: i.read_entropy_bits
+      ; shift_offset_next.reg <-- shift_offset_next.next.value
+      ; buffer.reg <-- buffer.next.value
+      ; one_byte_shifted.reg <-- one_byte_shifted.next.value
+      ; shift_offset <-- shift_offset_next.next.value.:[2, 0]
       ; if_
-          shift_offset_next.:(4)
+          shift_offset_next.next.value.:(4)
           [ (* >= 16 *)
-            jpeg_ready <-- vdd
+            read <-- vdd
           ; if_
-              one_byte_shifted.value
-              [ buffer <-- drop_bottom (drop_top buffer.value 16) 8 @: i.jpeg_in @: zero 8
+              one_byte_shifted.reg.value
+              [ buffer.next
+                <-- drop_bottom (drop_top buffer.reg.value 16) 8 @: i.jpeg_in @: zero 8
               ]
-              [ buffer <-- drop_top buffer.value 16 @: i.jpeg_in ]
+              [ buffer.next <-- drop_top buffer.reg.value 16 @: i.jpeg_in ]
           ]
         @@ elif
-             shift_offset_next.:(3)
+             shift_offset_next.next.value.:(3)
              [ (* >= 8 *)
                if_
-                 one_byte_shifted.value
-                 [ buffer <-- drop_bottom (drop_top buffer.value 8) 8 @: i.jpeg_in
-                 ; one_byte_shifted <-- gnd
-                 ; jpeg_ready <-- vdd
+                 one_byte_shifted.reg.value
+                 [ buffer.next
+                   <-- drop_bottom (drop_top buffer.reg.value 8) 8 @: i.jpeg_in
+                 ; one_byte_shifted.next <-- gnd
+                 ; read <-- vdd
                  ]
-                 [ buffer <-- drop_top buffer.value 8 @: zero 8
-                 ; one_byte_shifted <-- vdd
+                 [ buffer.next <-- drop_top buffer.reg.value 8 @: zero 8
+                 ; one_byte_shifted.next <-- vdd
                  ]
              ]
              []
+      ; sm.switch
+          [ ( Pass
+            , [ bits_valid <-- vdd
+              ; jpeg_ready <-- read.value
+              ; when_ read.value [ if_ i.jpeg_valid [] [ sm.set_next Load ] ]
+              ] )
+          ; ( Load
+            , [ jpeg_ready <-- vdd
+              ; bits_valid <-- gnd
+              ; when_ i.jpeg_valid [ sm.set_next Pass ]
+              ] )
+          ]
       ]);
-  { O.bits = sel_top bits 16; bits_valid = gnd; jpeg_ready = jpeg_ready.value }
+  { O.bits = sel_top bits 16
+  ; bits_valid = bits_valid.value
+  ; jpeg_ready = jpeg_ready.value
+  }
 ;;
